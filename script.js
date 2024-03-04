868895a05ab2b7ce0a1b6953c39d6c4685c1cef8
codebyellalesperance <ella.lesperance@outlook.com>
2024-03-04 16:54:21 -0500

    Merge remote changes

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// Contents of script.js
-const text = "software engineer."
-const typingSpeed = 110; // milliseconds
-let index = 0;
-const typingElement = document.getElementById('typing-effect');
+document.addEventListener('DOMContentLoaded', (event) => {
+    const text = "software engineer.";
+    const typingSpeed = 110; // milliseconds
+    let index = 0;
+    const typingElement = document.getElementById('typing-effect');
 
-function type() {
-    if (index < text.length) {
-        typingElement.innerHTML += text.charAt(index);
-        index++;
-        setTimeout(type, typingSpeed);
+    function type() {
+        if (index < text.length) {
+            typingElement.innerHTML += text.charAt(index);
+            index++;
+            setTimeout(type, typingSpeed);
+        }
     }
-}
 
-window.onload = () => {
-    typingElement.classList.add('typing');
-    type();
-};
+    if (typingElement) {
+        type();
+    }
+});
